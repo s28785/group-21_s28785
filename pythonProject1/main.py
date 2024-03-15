6872e6921948e9991dc697fb281335f20b309789
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2024-03-15 16:52:45 +0100

    done task 2

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -3,5 +3,10 @@ squares = [x**2 for x in range(1, 11)]
 print(squares)
 
 
+#task 2
+def generate_squares(start, end):
+    return [x**2 for x in range(start, end + 1)]
 
-
+#usage
+squares_example = generate_squares(1, 10)
+print(squares_example)
